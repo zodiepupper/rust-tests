9dda39b9a78ec59aefb16cb9ed8ffe4c86af0481
zodywoolsey <zodie@pupper.dev>
2023-12-24 14:04:01 -0700

    init stuff

---
+++ hello-cargo/target/debug/deps/hello_cargo-390f1fa4861e8c44.d
@@ -0,0 +1,5 @@
+/home/zodiepupper/Documents/github/rust-tests/hello-cargo/target/debug/deps/hello_cargo-390f1fa4861e8c44: src/main.rs
+
+/home/zodiepupper/Documents/github/rust-tests/hello-cargo/target/debug/deps/hello_cargo-390f1fa4861e8c44.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/home/zodiepupper/Documents/github/rust-tests/hello-cargo/target/debug/hello-cargo: /home/zodiepupper/Documents/github/rust-tests/hello-cargo/src/main.rs
